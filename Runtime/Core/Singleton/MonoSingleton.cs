6f7f67e0a5e1be0268d3bee84345715aa281c486
Sarofc <sarono@foxmail.com>
2022-11-08 23:36:45 +0800

    Update MonoSingleton.cs

--- Runtime/Core/Singleton/MonoSingleton.cs
+++ Runtime/Core/Singleton/MonoSingleton.cs
@@ -10,18 +10,7 @@ namespace Saro
      */
     public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
     {
-        /// <summary>
-        /// 获取实例
-        /// </summary>
-        /// <returns></returns>
-        [System.Obsolete("use \"Instance\" instead")]
-        public static T Get()
-        {
-            return Instance;
-        }
-
         public static T Instance { get; protected set; }
-
 
         protected virtual void Awake()
         {
@@ -32,9 +21,7 @@ namespace Saro
         protected virtual void OnDestroy()
         {
             if (Instance == this)
-            {
                 Instance = null;
-            }
         }
     }
 }
