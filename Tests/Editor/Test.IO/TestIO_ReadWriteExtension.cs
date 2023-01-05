b050172bcaad6b85645341491f090e84f12de11c
Sarofc <sarono@foxmail.com>
2023-01-05 21:15:39 +0800

    Update TestIO_ReadWriteExtension.cs

--- Tests/Editor/Test.IO/TestIO_ReadWriteExtension.cs
+++ Tests/Editor/Test.IO/TestIO_ReadWriteExtension.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Unity.Mathematics;
 using UnityEngine;
+
+using sfloat3 = ME.ECS.Mathematics.float3;
 
 namespace Saro.IO.Test
 {
@@ -38,14 +41,21 @@ namespace Saro.IO.Test
             },
         };
 
-        static IEnumerable<TestStruct> source_Struct = new List<TestStruct>
+        static IEnumerable<sfloat3> source_sfloat3 = new List<sfloat3>
         {
-            new TestStruct { a = 1, b = 1.1f, c = 1000u },
-            new TestStruct { a = 2, b = 2.2f, c = 2000u },
-            new TestStruct { a = 3, b = 3.3f, c = 3000u },
-            new TestStruct { a = 4, b = 4.4f, c = 4000u },
+            new sfloat3 (sfloat.FromRaw(1),sfloat.FromRaw(1),sfloat.FromRaw(1)),
+            new sfloat3 (sfloat.FromRaw(11),sfloat.FromRaw(12),sfloat.FromRaw(13)),
+            new sfloat3 (sfloat.FromRaw(122),sfloat.FromRaw(1),sfloat.FromRaw(1)),
+            new sfloat3 (sfloat.FromRaw(333),sfloat.FromRaw(444),sfloat.FromRaw(555)),
         };
 
+        static IEnumerable<(float3, bool)> source_float3_bool = new List<(float3, bool)>
+        {
+            (new float3 (1,2,3),                       true),
+            (new float3 (11,22,33),                  false),
+            (new float3 (1111,222,3333),        true),
+            (new float3 (12,23,34),                 false),
+        };
 
         static IEnumerable<int> source_Int = new List<int> { 1, 2, 3, 4 };
 
@@ -84,7 +94,24 @@ namespace Saro.IO.Test
         }
 
         [Test]
-        public void SerializeDeserialize_Struct([ValueSource(nameof(source_Struct))] TestStruct input)
+        public void SerializeDeserialize_sfloat3([ValueSource(nameof(source_sfloat3))] sfloat3 input)
+        {
+            var actual = input; // copy
+
+            using var ms = new MemoryStream();
+            using var writer = new BinaryWriter(ms);
+            writer.WriteUnmanaged(ref actual);
+
+            ms.Position = 0;
+            using var reader = new BinaryReader(ms);
+            reader.ReadUnmanaged(ref actual);
+
+            Assert.AreEqual(input, actual);
+        }
+
+
+        [Test]
+        public void SerializeDeserialize_float3_bool([ValueSource(nameof(source_float3_bool))] (float3, bool) input)
         {
             var actual = input; // copy
 
