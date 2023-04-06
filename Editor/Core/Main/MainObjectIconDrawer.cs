95c6e1f5e9b148231faf34e7fbd8c3c167c0135b
Sarofc <sarono@foxmail.com>
2023-04-06 19:37:20 +0800

    给Main.cs加一个图标

---
+++ Editor/Core/Main/MainObjectIconDrawer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Saro
+{
+    [InitializeOnLoad]
+    internal static class MainObjectIconDrawer
+    {
+        static MainObjectIconDrawer()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindowItemGUI;
+        }
+
+        static void OnHierarchyWindowItemGUI(int instanceID, Rect selectionRect)
+        {
+            var obj = EditorUtility.InstanceIDToObject(instanceID);
+            if (obj is GameObject go)
+            {
+                if (go.TryGetComponent<Main>(out var main))
+                {
+                    var objectContent = EditorGUIUtility.ObjectContent(main, typeof(Main));
+                    GUI.DrawTexture(new Rect(selectionRect.xMax - IconSize, selectionRect.yMin, IconSize, IconSize), objectContent.image);
+                }
+            }
+        }
+
+
+        static readonly int IconSize = 16;
+    }
+}
